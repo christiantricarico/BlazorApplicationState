599ddb7c9c51acf72b1c428662ee933c477c58a5
Christian Tricarico <christiantric@hotmail.it>
2023-01-21 17:57:20 +0100

    Add project files.

---
+++ BlazorApplicationState/Client/Services/AppState.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApplicationState.Client.Services
+{
+    public class AppState
+    {
+        public event Action<ComponentBase, string> StateChanged;
+
+        public string Message { get; private set; } = string.Empty;
+        public bool Enabled { get; private set; } = false;
+        public int Counter { get; private set; } = 0;
+
+        public void UpdateMessage(ComponentBase source, string message)
+        {
+            Message = message;
+            NotifyStateChanged(source, nameof(Message));
+        }
+
+        public void UpdateEnabled(ComponentBase source, bool enabled)
+        {
+            Enabled = enabled;
+            NotifyStateChanged(source, nameof(Enabled));
+        }
+
+        public void UpdateCounter(ComponentBase source, int counter)
+        {
+            Counter = counter;
+            NotifyStateChanged(source, nameof(Counter));
+        }
+
+        private void NotifyStateChanged(ComponentBase source, string property) => StateChanged?.Invoke(source, property);
+    }
+}
